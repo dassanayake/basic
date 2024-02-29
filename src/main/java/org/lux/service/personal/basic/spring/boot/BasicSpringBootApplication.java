80557f3944cd34d6588d7c05cc8e8eaa80ae375e
Lakmali Dassanayake <lakmali_dassanayake.adl@myboost.co>
2024-02-29 17:31:22 +0530

    initial commit

---
+++ src/main/java/org/lux/service/personal/basic/spring/boot/BasicSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.lux.service.personal.basic.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lux/service/personal/basic/spring/boot/BasicSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lux.service.personal.basic.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
